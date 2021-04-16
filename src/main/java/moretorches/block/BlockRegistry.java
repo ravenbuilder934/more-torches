21c350b5925e0489d09f11a7f9fb8383f81f16c5
Raven <sabrinajamie@outlook.com>
2021-04-16 13:11:04 -0500

    improve the sconce

--- src/main/java/moretorches/block/BlockRegistry.java
+++ src/main/java/moretorches/block/BlockRegistry.java
@@ -1,11 +1,8 @@
 package moretorches.block;
 
 import moretorches.MoreTorches;
-import net.minecraft.block.AbstractBlock;
-import net.minecraft.block.Block;
-import net.minecraft.block.SoundType;
+import net.minecraft.block.*;
 import net.minecraft.block.material.Material;
-import net.minecraft.particles.ParticleTypes;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -18,5 +15,5 @@ public class BlockRegistry
 
     public static final RegistryObject<Block> talltorch = BLOCKS.register("talltorch", () -> new Block(AbstractBlock.Properties.of(Material.DECORATION).lightLevel((p_235470_0_) -> { return 14; }).sound(SoundType.METAL)));
 
-    public static final RegistryObject<SconceBlock> sconce = BLOCKS.register("sconce", () -> new SconceBlock(AbstractBlock.Properties.of(Material.DECORATION).lightLevel((p_235469_0_) -> { return 14; }).sound(SoundType.WOOD), ParticleTypes.FLAME));
+    public static final RegistryObject<SconceBlock> sconce = BLOCKS.register("sconce", () -> new SconceBlock(AbstractBlock.Properties.of(Material.DECORATION).lightLevel((p_235469_0_) -> { return 10; }).sound(SoundType.WOOD)));
 }
--- src/main/java/moretorches/block/SconceBlock.java
+++ src/main/java/moretorches/block/SconceBlock.java
@@ -1,11 +1,8 @@
 package moretorches.block;
 
-import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.Maps;
 import net.minecraft.block.*;
 import net.minecraft.item.BlockItemUseContext;
-import net.minecraft.particles.IParticleData;
-import net.minecraft.particles.ParticleTypes;
+import net.minecraft.state.BooleanProperty;
 import net.minecraft.state.DirectionProperty;
 import net.minecraft.state.StateContainer;
 import net.minecraft.util.Direction;
@@ -19,93 +16,77 @@ import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
 import javax.annotation.Nullable;
-import java.util.Map;
 import java.util.Random;
 
-public class SconceBlock extends TorchBlock
+public class SconceBlock extends RedstoneTorchBlock
 {
     public static final DirectionProperty FACING = HorizontalBlock.FACING;
-    private static final Map<Direction, VoxelShape> AABBS = Maps.newEnumMap(ImmutableMap.of(Direction.NORTH, Block.box(5.5D, 3.0D, 11.0D, 10.5D, 13.0D, 16.0D), Direction.SOUTH, Block.box(5.5D, 3.0D, 0.0D, 10.5D, 13.0D, 5.0D), Direction.WEST, Block.box(11.0D, 3.0D, 5.5D, 16.0D, 13.0D, 10.5D), Direction.EAST, Block.box(0.0D, 3.0D, 5.5D, 5.0D, 13.0D, 10.5D)));
+    public static final BooleanProperty LIT = RedstoneTorchBlock.LIT;
 
-    public SconceBlock(AbstractBlock.Properties p_i241193_1_, IParticleData p_i241193_2_)
+    public SconceBlock(AbstractBlock.Properties p_i48341_1_)
     {
-        super(p_i241193_1_, p_i241193_2_);
-        this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH));
+        super(p_i48341_1_);
+        this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH).setValue(LIT, Boolean.valueOf(true)));
     }
 
     public VoxelShape getShape(BlockState p_220053_1_, IBlockReader p_220053_2_, BlockPos p_220053_3_, ISelectionContext p_220053_4_)
     {
-        return getShape(p_220053_1_);
-    }
-
-    public static VoxelShape getShape(BlockState p_220289_0_)
-    {
-        return AABBS.get(p_220289_0_.getValue(FACING));
+        return WallTorchBlock.getShape(p_220053_1_);
     }
 
     public boolean canSurvive(BlockState p_196260_1_, IWorldReader p_196260_2_, BlockPos p_196260_3_)
     {
-        Direction direction = p_196260_1_.getValue(FACING);
-        BlockPos blockpos = p_196260_3_.relative(direction.getOpposite());
-        BlockState blockstate = p_196260_2_.getBlockState(blockpos);
-        return blockstate.isFaceSturdy(p_196260_2_, blockpos, direction);
+        return Blocks.WALL_TORCH.canSurvive(p_196260_1_, p_196260_2_, p_196260_3_);
+    }
+
+    public BlockState updateShape(BlockState p_196271_1_, Direction p_196271_2_, BlockState p_196271_3_, IWorld p_196271_4_, BlockPos p_196271_5_, BlockPos p_196271_6_)
+    {
+        return Blocks.WALL_TORCH.updateShape(p_196271_1_, p_196271_2_, p_196271_3_, p_196271_4_, p_196271_5_, p_196271_6_);
     }
 
     @Nullable
     public BlockState getStateForPlacement(BlockItemUseContext p_196258_1_)
     {
-        BlockState blockstate = this.defaultBlockState();
-        IWorldReader iworldreader = p_196258_1_.getLevel();
-        BlockPos blockpos = p_196258_1_.getClickedPos();
-        Direction[] adirection = p_196258_1_.getNearestLookingDirections();
-
-        for(Direction direction : adirection)
-        {
-            if (direction.getAxis().isHorizontal())
-            {
-                Direction direction1 = direction.getOpposite();
-                blockstate = blockstate.setValue(FACING, direction1);
-                if (blockstate.canSurvive(iworldreader, blockpos))
-                {
-                    return blockstate;
-                }
-            }
-        }
-
-        return null;
-    }
-
-    public BlockState updateShape(BlockState p_196271_1_, Direction p_196271_2_, BlockState p_196271_3_, IWorld p_196271_4_, BlockPos p_196271_5_, BlockPos p_196271_6_)
-    {
-        return p_196271_2_.getOpposite() == p_196271_1_.getValue(FACING) && !p_196271_1_.canSurvive(p_196271_4_, p_196271_5_) ? Blocks.AIR.defaultBlockState() : p_196271_1_;
+        BlockState blockstate = Blocks.WALL_TORCH.getStateForPlacement(p_196258_1_);
+        return blockstate == null ? null : this.defaultBlockState().setValue(FACING, blockstate.getValue(FACING));
     }
 
     @OnlyIn(Dist.CLIENT)
     public void animateTick(BlockState p_180655_1_, World p_180655_2_, BlockPos p_180655_3_, Random p_180655_4_)
     {
-        Direction direction = p_180655_1_.getValue(FACING);
-        double d0 = (double)p_180655_3_.getX() + 0.5D;
-        double d1 = (double)p_180655_3_.getY() + 0.7D;
-        double d2 = (double)p_180655_3_.getZ() + 0.5D;
-        double d3 = 0.22D;
-        double d4 = 0.27D;
-        Direction direction1 = direction.getOpposite();
-        p_180655_2_.addParticle(ParticleTypes.SMOKE, d0 + 0.27D * (double)direction1.getStepX(), d1 + 0.22D, d2 + 0.27D * (double)direction1.getStepZ(), 0.0D, 0.0D, 0.0D);
-        p_180655_2_.addParticle(this.flameParticle, d0 + 0.27D * (double)direction1.getStepX(), d1 + 0.22D, d2 + 0.27D * (double)direction1.getStepZ(), 0.0D, 0.0D, 0.0D);
+        if (p_180655_1_.getValue(LIT)) {
+            Direction direction = p_180655_1_.getValue(FACING).getOpposite();
+            double d0 = 0.27D;
+            double d1 = (double)p_180655_3_.getX() + 0.5D + (p_180655_4_.nextDouble() - 0.5D) * 0.2D + 0.27D * (double)direction.getStepX();
+            double d2 = (double)p_180655_3_.getY() + 0.7D + (p_180655_4_.nextDouble() - 0.5D) * 0.2D + 0.22D;
+            double d3 = (double)p_180655_3_.getZ() + 0.5D + (p_180655_4_.nextDouble() - 0.5D) * 0.2D + 0.27D * (double)direction.getStepZ();
+            p_180655_2_.addParticle(this.flameParticle, d1, d2, d3, 0.0D, 0.0D, 0.0D);
+        }
+    }
+
+    protected boolean hasNeighborSignal(World p_176597_1_, BlockPos p_176597_2_, BlockState p_176597_3_)
+    {
+        Direction direction = p_176597_3_.getValue(FACING).getOpposite();
+        return p_176597_1_.hasSignal(p_176597_2_.relative(direction), direction);
+    }
+
+    public int getSignal(BlockState p_180656_1_, IBlockReader p_180656_2_, BlockPos p_180656_3_, Direction p_180656_4_)
+    {
+        return p_180656_1_.getValue(LIT) && p_180656_1_.getValue(FACING) != p_180656_4_ ? 15 : 0;
     }
 
     public BlockState rotate(BlockState p_185499_1_, Rotation p_185499_2_)
     {
-        return p_185499_1_.setValue(FACING, p_185499_2_.rotate(p_185499_1_.getValue(FACING)));
+        return Blocks.WALL_TORCH.rotate(p_185499_1_, p_185499_2_);
     }
 
     public BlockState mirror(BlockState p_185471_1_, Mirror p_185471_2_)
     {
-        return p_185471_1_.rotate(p_185471_2_.getRotation(p_185471_1_.getValue(FACING)));
+        return Blocks.WALL_TORCH.mirror(p_185471_1_, p_185471_2_);
     }
 
     protected void createBlockStateDefinition(StateContainer.Builder<Block, BlockState> p_206840_1_)
     {
-        p_206840_1_.add(FACING);
+        p_206840_1_.add(FACING, LIT);
     }
 }
