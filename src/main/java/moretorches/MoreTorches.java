2739a34e0a544cfcc1481e8ee4bb7f54038b19d0
Raven <sabrinajamie@outlook.com>
2021-05-15 22:12:32 -0500

    fixit!

--- src/main/java/moretorches/MoreTorches.java
+++ src/main/java/moretorches/MoreTorches.java
@@ -2,9 +2,15 @@ package moretorches;
 
 import moretorches.block.BlockRegistry;
 import moretorches.item.ItemRegistry;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.RenderTypeLookup;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
@@ -17,6 +23,7 @@ public class MoreTorches
     {
         IEventBus modBus = FMLJavaModLoadingContext.get().getModEventBus();
         modBus.addListener(this::setup);
+        modBus.addListener(this::doClientStuff);
         ItemRegistry.ITEMS.register(modBus);
         BlockRegistry.BLOCKS.register(modBus);
     }
@@ -24,6 +31,15 @@ public class MoreTorches
     private void setup(final FMLCommonSetupEvent event)
     {
         MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    @SubscribeEvent
+    public void doClientStuff(final FMLClientSetupEvent event)
+    {
+        RenderTypeLookup.setRenderLayer(BlockRegistry.lecterntorch.get(), RenderType.translucent());
+        RenderTypeLookup.setRenderLayer(BlockRegistry.talltorch.get(), RenderType.translucent());
+        RenderTypeLookup.setRenderLayer(BlockRegistry.sconce.get(), RenderType.translucent());
     }
 
 }
